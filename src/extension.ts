2afd0eefc5f42ea96d7895a2242952c11180effa
Edward Pasenidis <me@edpasenidis.tech>
2020-07-05 03:42:54 +0300

    feat: add search / import function

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import fetch, { Headers } from 'node-fetch';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "google-fonts-instant-import" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('google-fonts-instant-import.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let search = vscode.commands.registerCommand('google-fonts-instant-import.searchFont', async () => {
+		const prompt: string = String(await vscode.window.showInputBox({ placeHolder: 'Enter font (family) name (e.g Open Sans)' }));
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from google-fonts-instant-import!');
+		const response = await fetch('https://cors-anywhere.herokuapp.com/https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=AIzaSyDrwscy04xGYMeRyeWOnxXilRnyCafwqHA', {
+			headers: new Headers({
+				'origin': 'thomaspark.co'
+			})
+		});
+
+		const data = await response.json();
+
+		const fonts = data.items;
+		let font: { family: string; } = { family: '' };
+
+		for (let i = 0; i < fonts.length; i++) {
+			if (fonts[i].family === prompt) {
+				font = fonts[i];
+				break;
+			} else {
+				continue;
+			}
+		}
+
+		vscode.window.showInformationMessage(`Font is ${font.family}`);
+
+			const href = `https://fonts.googleapis.com/css2?family=${font.family.split(' ').join('+')}:wght@300&display=swap`;
+			vscode.window.showInformationMessage(href);
+		// }
+
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+			
+			editor.edit(editBuilder => {
+				editBuilder.replace(selection, document.getText(selection) + `\n\t<link rel="stylesheet" href="${href}">\n\t<style>\n\t\tbody { font-family: '${font.family}', sans-serif; }\n\t</style>`);
+			});
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(search);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
